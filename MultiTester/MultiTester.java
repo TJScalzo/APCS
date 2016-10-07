6dffd7074e0b3104be41a4605ad6c9774b6a8120
TJScalzo <timtv25@gmail.com>
2016-10-07 09:44:09 -0400

    Finished multitester.

--- MultiTester/MultiTester.java
+++ MultiTester/MultiTester.java
@@ -2,6 +2,7 @@ public class MultiTester
 {
     public static void main(String[] args)
     {
+        // Bank Account Stuff
         BankAccount b1 = new BankAccount();
         BankAccount b2 = new BankAccount(999.99, "Roxanne");
         BankAccount b3 = new BankAccount("'Broke' Joe");
@@ -10,19 +11,38 @@ public class MultiTester
         b1.printInfo();
         b1.withdraw(1000);
         b1.printInfo();
+        System.out.println();
         
         b2.deposit(35);
         b2.printInfo();
         b2.withdraw(1256);
         b2.printInfo();
+        System.out.println();
         
         b3.deposit(29669);
         b3.printInfo();
         b3.withdraw(100);
         b3.printInfo();
+        System.out.println();
         
-        
+        // Employee Stuff
         Employee e1 = new Employee("Jane", "Eyre", 17, "Old Maid", 0.0);
         
+        //Methods that don't show anything in console:
+        e1.getFirst();
+        e1.getLast();
+        e1.getID();
+        
+        //Methods that show stuff in console:
+        e1.setID("12-345-6789");
+        e1.increaseAge();
+        e1.giveRaise(0.75);
+        e1.printPersonnelReport();
+        System.out.println();
+        e1.changePosition("Wife", 0.0);
+        e1.printPersonnelReport();
+        System.out.println();
+        e1.fire();
+        e1.printPersonnelReport();
     }
 }
