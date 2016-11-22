c918ddf474f89f8ce54ef2b8da14a232c06e52c1
TJScalzo <timtv25@gmail.com>
2016-11-22 18:36:29 -0500

    Completed the 2nd part of practice

---
+++ General Practice/Person.java
@@ -0,0 +1,128 @@
+public class Person
+{
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private double weight; 
+    private int IQ;
+    private String birthDate;
+    private boolean isFemale;
+    private int serial;
+    private static int nextNum = 0;
+    
+    public Person()
+    {
+        firstName = "Sam";
+        lastName = "Java";
+        age = 21;
+        height = 66.0;
+        weight = 175.0;
+        IQ = 100; 
+        birthDate = "07-31-2000";
+        isFemale = (Math.random() > 0.5);
+        serial = nextNum;
+        nextNum++;
+    }
+    public Person(String firstName, String lastName, int age, double height, double weight, int IQ, String birthDate, boolean isFemale)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.IQ = IQ; 
+        this.birthDate = birthDate;
+        this.isFemale = isFemale;
+        serial = nextNum;
+        nextNum++;
+    }
+
+        public String getFirstName()
+    {
+        return this.firstName;
+    }
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+    public int getAge()
+    {
+        return this.age;
+    }
+    public double getWeight()
+    {
+        return this.weight;
+    }
+    public double getHeight()
+    {
+        return this.height;
+    }
+    public int getIQ()
+    {
+        return this.IQ;
+    }
+    public String getBirthDate()
+    {
+        return this.birthDate;
+    }
+    public boolean isPersonFemale()
+    {
+        return this.isFemale;
+    }
+    public void lernStuf()
+    {
+        this.IQ = this.IQ + 1;
+    }
+    public void sniffMarker(int howLong)
+    {
+        this.IQ = this.IQ - (int)(howLong * 1.02);
+    }
+    public void printPersonalReport()
+    {
+        System.out.println("Name:" + " " + firstName + " " + lastName);
+        System.out.println("Age:" + " " + age);
+        System.out.println("Height:" + " " + height);
+        System.out.println("Weight:" + " " + weight);
+        System.out.println("IQ:" + " " + IQ);
+        System.out.println("Birthdate:" + " " + birthDate);
+        if (!isFemale) {
+            System.out.println("The person is male.");
+        } else {
+            System.out.println("The person is female.");
+        }
+    }
+    public Person createChild(Person partner, String firstName, String birthDate)
+    {
+        Person mother;
+        Person father;
+        Person child = null;
+        if (child != null) {
+            System.out.println("Excuse me, you've already had a child. Leave some room for the rest of us!");
+            return null;
+        } else {
+            if (partner.isFemale == this.isFemale) {
+                System.out.println("Sorry, you can't create a child on your own.");
+                return null;
+            } else {
+                if (this.isFemale) {
+                    mother = this;
+                    father = partner;
+                } else {
+                    mother = partner;
+                    father = this;
+                }
+                boolean isFemale = (Math.random() > 0.5);
+                child = new Person(firstName, father.lastName, 0, 20, 7.5, mother.IQ, birthDate, isFemale);
+                String childNoun;
+                if (child.isFemale) {
+                    childNoun = "girl";
+                } else {
+                    childNoun = "boy";
+                }
+                System.out.println("Congratulations, it's a " + childNoun + "!");
+                return child;
+            }
+        }
+    }
+}
