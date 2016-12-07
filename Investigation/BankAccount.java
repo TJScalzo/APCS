8a94be928a26bc7ff313192a013f5d3625b5491d
TJScalzo <timtv25@gmail.com>
2016-12-07 09:47:52 -0500

    Added BankAccount to project for doubleTrouble stuff

---
+++ Investigation/BankAccount.java
@@ -0,0 +1,54 @@
+public class BankAccount
+{
+    // Fields
+    private double balance;
+    private String owner;
+    // Constructors
+    public BankAccount() // Default constructor
+    {
+        balance = 0.00;
+        owner = "Mary Sue";
+    }
+    public BankAccount(double b, String o) // Some other constructor
+    {
+        balance = b;
+        owner = o;
+    }
+    public BankAccount(String o)
+    {
+        balance = 0.00;
+        owner = o;
+    }
+    public BankAccount(double b)
+    {
+        balance = b;
+        owner = "Mary Sue";
+    }
+    // Methods
+    public double getBalance()
+    {
+        return balance;
+    }
+    public String getOwner()
+    {
+        return owner;
+    }
+    public void deposit(double howmuch)
+    {
+        balance = balance + howmuch;
+    }
+    public void withdraw(double howmuch)
+    {
+        balance = balance - howmuch;
+    }
+    public void transfer(BankAccount sendingTo, double amount)
+    {
+        this.withdraw(amount);
+        sendingTo.deposit(amount);
+    }
+    public void printInfo()
+    {
+        System.out.println("Owner: " + owner);
+        System.out.println("Balance: $" + balance);
+    }
+}
