344bf06fc0d24bc741ffe0f3dfac451b8eb71e1d
TJScalzo <timtv25@gmail.com>
2016-12-11 17:09:37 -0500

    Added Randomness

---
+++ Investigation/Randomness.java
@@ -0,0 +1,9 @@
+public class Randomness
+{
+    public static void main(String[] args)
+    {
+        while (true) {
+            System.out.println((int)(10*Math.random())+5);
+        }
+    }
+}
