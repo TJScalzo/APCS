6e9c06629147fdc5a87b6785c5a1adac9e970fef
TJScalzo <timtv25@gmail.com>
2017-01-11 09:22:12 -0500

    Fixed indentations

--- Cryptography/Message.java
+++ Cryptography/Message.java
@@ -1,23 +1,23 @@
 public class Message
 {
-	private boolean isCiphered;
-	private String statement;
-	public Message()
-	{
-		isCiphered = false;
-		statement = "hello world";
-	}
-	public Message(String statement, boolean isCiphered)
-	{
-		this.isCiphered = isCiphered;
-		this.statement = statement;
-	}
-	public String returnStatement()
-	{
-		return statement;
-	}
-	public boolean returnIsCiphered()
-	{
-		return isCiphered;
-	}
+    private boolean isCiphered;
+    private String statement;
+    public Message()
+    {
+        isCiphered = false;
+        statement = "hello world";
+    }
+    public Message(String statement, boolean isCiphered)
+    {
+        this.isCiphered = isCiphered;
+        this.statement = statement;
+    }
+    public String returnStatement()
+    {
+        return statement;
+    }
+    public boolean returnIsCiphered()
+    {
+        return isCiphered;
+    }
 }
