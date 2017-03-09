9d544474bbf1a646349142464327fe6b92b11b08
TJScalzo <timtv25@gmail.com>
2017-03-09 09:51:18 -0500

    Created Fibonacci Group class

--- Practice_with_Interfaces/FibonacciGroup.java
+++ Practice_with_Interfaces/FibonacciGroup.java
@@ -5,7 +5,20 @@ public class FibonacciGroup implements NumberGroup
     
     public FibonacciGroup(int length)
     {
-        //asdf
+        this.length = length;
+        group = new int[length];
+        int prev2 = 0;
+        int prev1 = 1;
+        for(int i = 0; i < length; i++) {
+            if(i <= 1) {
+                group[0] = prev2;
+                group[1] = prev1;
+            } else {
+                group[i] = prev2 + prev1;
+                prev2 = prev1;
+                prev1 = group[i];
+            }
+        }
     }
     
     public boolean contains(int num)
